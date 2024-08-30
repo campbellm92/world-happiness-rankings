76c3a0e868d8fad9fa00a4c87ea3c459b9360004
Matthew Campbell <m.campbell92@gmail.com>
2024-08-30 18:47:39 +0200

    import carousel component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,11 @@
+import HeroCarousel from "./Carousel";
+
 function Home() {
-  return <main className="flex-grow-1"></main>;
+  return (
+    <main className="flex-grow-1">
+      <HeroCarousel />
+    </main>
+  );
 }
 
 export default Home;
