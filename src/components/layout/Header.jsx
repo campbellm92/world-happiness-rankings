e6b51618ab10064096981f8852c95129d3159e44
Matthew Campbell <m.campbell92@gmail.com>
2024-08-30 09:11:57 +0200

    experiment with diff ways of targeting nav links in css

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,22 +1,43 @@
+import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import { Nav, Navbar } from "react-bootstrap/";
+import logo from "../../assets/images/logo2.svg";
 
 function Header() {
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container fluid className="d-flex justify-content-between">
-          <Navbar.Brand href="#home" className="ms-3 brand-custom">
-            World Happiness Rankings
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/signup" className="nav-link-custom">
-              Sign up
-            </Nav.Link>
-            <Nav.Link href="/login" className="nav-link-custom">
-              Login
-            </Nav.Link>
-          </Nav>
+      <Navbar bg="background" data-bs-theme="dark" expand="md">
+        <Container fluid>
+          <div className="d-flex justify-content-between flex-wrap">
+            <Navbar.Brand as={NavLink} to="/" className="ms-3">
+              {/* <Navbar.Text className="text-wrap brand-custom">
+                World Happiness Rankings
+              </Navbar.Text> */}
+              <img className="logo" src={logo} alt="logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </div>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Nav.Link
+                as={NavLink}
+                to="/signup"
+                className="nav mx-2 text-accent nav-link-custom"
+              >
+                SIGN UP
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                to="/login"
+                className="nav mx-2 text-accent nav-link-custom"
+              >
+                LOGIN
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
