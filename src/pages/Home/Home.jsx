d13bf109b9e0c43473b78de9699fc3e797531c06
Matthew Campbell <m.campbell92@gmail.com>
2024-09-02 15:24:37 +0200

    import test hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import HeroImage from "./Hero";
 import HeroCarousel from "./Carousel";
 
 function Home() {
   return (
     <main className="flex-grow-1">
+      <HeroImage />
       <HeroCarousel />
     </main>
   );
