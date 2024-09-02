7d0eea08d1f8d43251b32c8375478ce0819a54e6
Matthew Campbell <m.campbell92@gmail.com>
2024-09-02 16:23:12 +0200

    import rankings endpoint

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroImage from "./Hero";
 import HeroCarousel from "./Carousel";
+import Rankings from "./Rankings";
 
 function Home() {
   return (
     <main className="flex-grow-1">
       <HeroImage />
       <HeroCarousel />
+      <Rankings />
     </main>
   );
 }
