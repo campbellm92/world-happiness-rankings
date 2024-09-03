bc3306f3f73bebe86283b4ae3d1c305586b893fd
Matthew Campbell <m.campbell92@gmail.com>
2024-09-03 18:02:30 +0200

    import hero, remove default styling

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,31 +1,16 @@
 function HeroImage() {
   return (
-    <header style={{ paddingLeft: 0 }}>
-      <div
-        className="p-5 text-center bg-image"
-        style={{
-          backgroundImage:
-            "url('https://mdbootstrap.com/img/new/slides/041.webp')",
-          height: 400,
-        }}
-      >
-        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
-          <div className="d-flex justify-content-center align-items-center h-100 w-100">
-            <div className="text-white">
-              <h1 className="mb-3">Heading</h1>
-              <h4 className="mb-3">Subheading</h4>
-              <a
-                className="btn btn-outline-light btn-lg"
-                href="#!"
-                role="button"
-              >
-                Call to action
-              </a>
-            </div>
-          </div>
+    <div className="p-5 text-center bg-image hero-custom">
+      <div className="d-flex justify-content-center align-items-center h-100 w-100">
+        <div className="p-5 rounded hero-mask">
+          <h1 className="mb-3 hero-header">World Happiness Report</h1>
+          <h4 className="mb-3 hero-sub-header">
+            Results for the World Happiness Report are in. Explore our data
+            story below and see how your country and other countries fared.
+          </h4>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
