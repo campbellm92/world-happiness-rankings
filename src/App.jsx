25b551b7d79c1634effc52327563532d5451507a
Matthew Campbell <m.campbell92@gmail.com>
2024-09-03 18:01:21 +0200

    footer commented out for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/styles.css";
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
 import Home from "./pages/Home/Home";
 import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+// import Footer from "./components/layout/Footer";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 
@@ -13,14 +13,14 @@ function App() {
     <BrowserRouter>
       <div className="d-flex flex-column min-vh-100">
         <Header />
-        <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Container>
-        <Footer />
+        {/* <Container> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        {/* </Container> */}
+        {/* <Footer /> */}
       </div>
     </BrowserRouter>
   );
