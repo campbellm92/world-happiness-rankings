3daaf71863e2ab443eb8e914c71a12f8c1488560
Matthew Campbell <m.campbell92@gmail.com>
2024-09-12 17:41:49 +0200

    change text

--- src/pages/Home/Carousel.jsx
+++ src/pages/Home/Carousel.jsx
@@ -6,7 +6,9 @@ function HeroCarousel() {
       <Carousel>
         <Carousel.Item className="carousel-item-custom one">
           <Carousel.Caption className="carousel-caption-custom">
-            <h3>Which are the happiest countries in the world?</h3>
+            <h3 className="carousel-header-custom">
+              Which are the happiest countries in the world?
+            </h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             <Button className="carousel-button-custom one">
               Find out more
@@ -15,7 +17,9 @@ function HeroCarousel() {
         </Carousel.Item>
         <Carousel.Item className="carousel-item-custom two">
           <Carousel.Caption className="carousel-caption-custom">
-            <h3>What makes a country happy?</h3>
+            <h3 className="carousel-header-custom">
+              What are the current trends?
+            </h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <Button className="carousel-button-custom two">
               Find out more
@@ -24,7 +28,9 @@ function HeroCarousel() {
         </Carousel.Item>
         <Carousel.Item className="carousel-item-custom three">
           <Carousel.Caption className="carousel-caption-custom">
-            <h3>Third slide label</h3>
+            <h3 className="carousel-header-custom">
+              What factors contribute to happiness?
+            </h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
