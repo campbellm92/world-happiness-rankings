f46bf883961de6421e6a980afacc51a7de8a408a
Matthew Campbell <m.campbell92@gmail.com>
2024-09-14 12:42:30 +0200

    add logic for checking auth, add logging

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,8 @@
 import { useState, useEffect, useRef } from "react";
+import fetchWithAuth from "../utils/fetchWithAuth";
 
-function useFetch(url) {
-  const cache = useRef({});
+function useFetch(url, requiresAuth = false) {
+  // const cache = useRef({});
   const [data, setData] = useState([]);
   const [fetchPending, setFetchPending] = useState(false);
   const [error, setError] = useState(null);
@@ -9,40 +10,52 @@ function useFetch(url) {
 
   useEffect(() => {
     if (!url) return;
-    const controller = new AbortController();
-    const { signal } = controller;
+
+    console.log("Fetching data from URL:", url); // Add this
+    console.log("Headers being sent:", headers);
+    // const controller = new AbortController();
+    // const { signal } = controller;
 
     const fetchData = async () => {
       setFetchPending(true);
       try {
-        if (cache.current[url]) {
-          const data = cache.current[url];
-          setData(data);
-          setFetchPending(false);
-        } else {
-          const response = await fetch(url, {
-            signal,
-            headers: { ...headers },
-          });
-          if (!response.ok) throw new Error(response.statusText);
-          const data = await response.json();
-          cache.current[url] = data;
-          setFetchPending(false);
-          setData(data);
-          setError(null);
+        // if (cache.current[url]) {
+        //   setData(cache.current[url]);
+        //   setFetchPending(false);
+        //   return;
+        // }
+
+        const response = requiresAuth
+          ? await fetchWithAuth(url, { headers }) // REMOVED SIGNAL (ABORTCONTROLLER) FOR NOW _ CONSIDER PLACING BACK
+          : await fetch(url, { headers }); // REMOVED SIGNAL FOR NOW
+
+        if (!response.ok) {
+          console.error(`Fetch failed: Status ${response.status}`);
+          const errorData = await response.json().catch(() => null);
+          console.error("Response data:", errorData);
+          throw new Error(`Network error: ${response.status}`);
         }
+
+        const fetchedData = await response.json();
+        // cache.current[url] = fetchedData;
+        setData(fetchedData);
+        setError(null);
       } catch (error) {
         if (error.name !== "AbortError") {
           setError(`${error} Could not fetch data`);
-          setFetchPending(false);
         }
+      } finally {
+        setFetchPending(false);
       }
     };
+
     fetchData();
-    return () => {
-      controller.abort();
-    };
-  }, [url, headers]);
+
+    // return () => {
+    //   controller.abort();
+    // };
+  }, [url, headers, requiresAuth, fetchWithAuth]);
+
   return { data, fetchPending, error, setHeaders };
 }
 
