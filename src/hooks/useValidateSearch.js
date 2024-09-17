037eb0a98752df7d8d032e8b48745447c8aac35e
Matthew Campbell <m.campbell92@gmail.com>
2024-09-17 16:51:37 +0200

    modularise search validation logic into hook

---
+++ src/hooks/useValidateSearch.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
+
+function useValidateSearch(country, year, limit) {
+  const [error, setError] = useState("");
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const countriesURL = "https://d2h6rsg43otiqk.cloudfront.net/prod/countries";
+
+  const {
+    data: countriesData = [],
+    error: countriesError,
+    setHeaders: setCountriesHeaders,
+  } = useFetch(countriesURL);
+
+  useEffect(() => {
+    setCountriesHeaders({
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    });
+  }, [setCountriesHeaders]);
+
+  const validateInputs = () => {
+    setError("");
+
+    const yearNumber = parseInt(year);
+    if (yearNumber < 2015 || yearNumber > 2020) {
+      setError("Please enter a year between 2015 and 2020.");
+      return false;
+    }
+
+    const limitNumber = parseInt(limit);
+    if (limitNumber < 1 || limitNumber > 160) {
+      setError("Please enter a results limit between 1 and 160");
+      return false;
+    }
+
+    if (!countriesData || countriesData.length === 0) {
+      setError("Countries list is still loading. Please wait...");
+      return false;
+    }
+
+    if (country.trim() !== "") {
+      if (
+        !countriesData
+          .map((c) => c.toLowerCase())
+          .includes(country.trim().toLowerCase())
+      ) {
+        setError("Please enter a valid country name.");
+        return false;
+      }
+    }
+
+    if (countriesError) {
+      setError("Failed to load countries. Please try again later.");
+      return false;
+    }
+
+    setError("");
+    return true;
+  };
+
+  return { validateInputs, error, countriesError };
+}
+
+export default useValidateSearch;
